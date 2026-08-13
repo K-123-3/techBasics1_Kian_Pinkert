8b20be5f24602c7c5787bafefab340b7fcfd4109
K-123-3 <kian.pinkert@stud.leuphana.de>
2026-08-13 16:52:27 +0200

    - city part of map started - figured out cameras (scrolling background) - more bikes

---
+++ final_project/map/pixil-frame-0 19.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="pixil-frame-0 19" tilewidth="160" tileheight="160" tilecount="136" columns="17">
+ <image source="../../../../Downloads/pixil-frame-0 19.png" width="2800" height="1400"/>
+</tileset>
